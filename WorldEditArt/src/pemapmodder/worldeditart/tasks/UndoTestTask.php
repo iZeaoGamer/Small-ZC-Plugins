9e608dd2dda7bce488e67db7a06ca4a38d82070c
iZeaoGamer <jameshayward14@icloud.com>
2018-09-01 14:49:26 +0100

    Update UndoTestTask.php

--- WorldEditArt/src/pemapmodder/worldeditart/tasks/UndoTestTask.php
+++ WorldEditArt/src/pemapmodder/worldeditart/tasks/UndoTestTask.php
@@ -4,18 +4,17 @@ namespace pemapmodder\worldeditart\tasks;
 
 use pemapmodder\worldeditart\utils\spaces\Space;
 use pemapmodder\worldeditart\WorldEditArt;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 
-class UndoTestTask extends PluginTask{
+class UndoTestTask extends Task{
 	/** @var Space */
 	private $space;
 
 	public function __construct(WorldEditArt $main, Space $space){
-		parent::__construct($main);
 		$this->space = clone $space;
 	}
 
-	public function onRun($ticks){
+	public function onRun(int $ticks) : void{
 //		$this->space->undoLastTest();
 	}
 
